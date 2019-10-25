aa44a4b5d171300a8dce068d3ea2535e9e37804c
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-10-25 14:30:56 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Game
-  attr_accessor :board, :player_1, :player_2, :cells#, :human
+  attr_accessor :board, :player_1, :player_2, :cells
 
   WIN_COMBINATIONS = [
   [0,1,2],
@@ -90,37 +90,33 @@ end
 
 def call
 input = ""
-while input != 'exit'
-  puts "Welcome to a game of Tic-tac-toe!"
-  puts "To play a game against a friend enter '2'."
-  puts "To play a game against the computer enter '1'."
-  puts "To see the computer play against itself enter '0'."
-  input = gets.strip.downcase
-  case input
-    when '2'
-      Game.new(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board= Board.new)
-      play
-      #call
-    when '1'
-      puts "Who should go first and be 'X'?"
-      puts "To choose computer type '1'."
-      puts "To choose yourself type '2'."
-      input2 = gets.strip.downcase
-        if input2 =="1"
-        computerVsMan
+  while input != 'exit'
+    puts "Welcome to a game of Tic-tac-toe!"
+    puts "To play a game against a friend enter '2'."
+    puts "To play a game against the computer enter '1'."
+    puts "To see the computer play against itself enter '0'."
+    input = gets.strip.downcase
+    case input
+      when '2'
+        Game.new(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board= Board.new)
         play
-        #call
-        elsif input2 =="2"
-        manVsComputer
+      when '1'
+        puts "Who should go first and be 'X'?"
+        puts "To choose computer type '1'."
+        puts "To choose yourself type '2'."
+        input2 = gets.strip.downcase
+          if input2 =="1"
+            computerVsMan
+            play
+
+          elsif input2 =="2"
+            manVsComputer
+            play
+          end
+      when '0'
+        computerVsComputer
         play
-        #call
-        end
-    when '0'
-      computerVsComputer
-      play
-      #call
+      end
     end
-    call
   end
-end
 end
